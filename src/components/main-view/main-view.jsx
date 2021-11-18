84159b0118e6e7a31539eb7fb1355020d60137a4
supern0va1977 <85573435+supern0va1977@users.noreply.github.com>
2021-11-17 21:34:01 -0600

    start of lesson 3.4

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,13 +9,21 @@ export class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
-      movies: [
-        { _id: 1, Title: 'Avatar', Description: 'A paraplegic Marine dispatched to the moon Pandora on a unique mission', ImagePath: 'https://m.media-amazon.com/images/I/51TIUdSaBUL._SY445_.jpg'},
-        { _id: 2, Title: 'Transformers', Description: 'An ancient struggle between two Cybertronian races', ImagePath: 'https://m.media-amazon.com/images/I/51REhA8e-gL._SY445_.jpg'},
-        { _id: 3, Title: 'Prometheus', Description: 'Following clues to the origin of mankind a team finds a structure', ImagePath: 'https://m.media-amazon.com/images/I/91mgd1UYdvL._SX342_.jpg'}
-      ],
+      movies: [],
       selectedMovie: null
     };
+  }
+
+  componentDidMount(){
+    axios.get('https://movie-builder-app.herokuapp.com/movies')
+      .then(response => {
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -25,14 +35,14 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
 
-    if (movies.length === 0) return <div className = "main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className = "main-view" />;
     
     return (
       <div className="main-view">
         {selectedMovie
           ? <MovieView movie = {selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
           : movies.map(movie => (
-            <MovieCard key = {movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+            <MovieCard key = {movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }} />
             ))
         }
       </div>
